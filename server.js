a72d5e040eeb184970eaa3faca77e07aa1274bbc
AVIX <avix256@gmail.com>
2022-11-17 20:24:31 +0700

    origins: *

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ const port = process.env.PORT || 4000
 const app = require('./app')
 const server = app.listen(port)
 
-var io = require('socket.io').listen(server)
+var io = require('socket.io').listen(server, { origins: '*:*'})
 require('./src/ioManager')(io)
